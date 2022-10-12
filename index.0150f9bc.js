57012b67798311d06f8e152e1c05f6f3b40d36cb
Volodymyr-Mospan <109673202+Volodymyr-Mospan@users.noreply.github.com>
2022-10-12 12:41:36 +0000

    Deploying to gh-pages from @ Volodymyr-Mospan/command-project-html-css-fson64@5a58cd20012f86f38cb1458514f817758385c7c7 🚀

---
+++ index.0150f9bc.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".customer").slick({dots:!0,infinite:!0,arrows:!1,slidesToShow:1,slidesToScroll:1})}));
+//# sourceMappingURL=index.0150f9bc.js.map
---
+++ index.de062370.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".customer").slick({dots:!0,infinite:!0,arrows:!1,slidesToShow:1,slidesToScroll:1})}));
+//# sourceMappingURL=index.de062370.js.map
